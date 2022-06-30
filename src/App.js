06529ee1e945eef866ad8a83c6d07eabae8e86c7
HeraTheYorkie <nikolina.pavic@gmail.com>
2022-06-30 18:17:59 +0200

    Remove counter, ppasword

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import Button from "./components/Button";
 import FormError from "./components/FormError";
 import FormMessage from "./components/FormMessage";
 import { User } from "./components/User";
-import { Counter } from "./components/Counter";
 import { useState } from "react";
 
 function App() {
-  const [formState, setFormState] = useState({ username: '', password: '' });
+  const [formState, setFormState] = useState({ username: '' });
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
@@ -16,16 +15,14 @@ function App() {
     event.preventDefault();
     console.log(formState);
 
-
-    if (formState.password === 'password') {
+    if (formState.username === '') {
+      setError(true);
+    } else {
       setUser(formState);
       setError(null);
-    } else {
-      setError(true);
     }
   }
   
-
   const handleChange = (event) => {
     console.log(event);
     setFormState((state) => ({
@@ -45,16 +42,11 @@ function App() {
 
   return (
     <div>
-      <Counter initialValue={0} />
-      <Counter initialValue={100} />
       {isSignedIn && <Button type="button" onClick={signOut}>Sign out</Button>}
       {isSignedIn && <User src="/avatar.jpg" username={user.username} />}
       {!isSignedIn && <form className="form" onSubmit={handleSubmit}>
         <div className="form-field">
           <InputElement name="username" label="Username" type="text" onChange={handleChange} />
-        </div>
-        <div className="form-field">
-          <InputElement name="password" label="Password" type="password" onChange={handleChange} />
         </div>
         <div className="form-field">
           <Button type="submit">Sign in</Button>
