8d0e5ecca9a2a2c08642aa9023b5dcd27f013a26
HeraTheYorkie <nikolina.pavic@gmail.com>
2022-07-05 15:14:55 +0200

    changes day 8

--- src/pages/ChatRoomPage.jsx
+++ src/pages/ChatRoomPage.jsx
@@ -2,47 +2,72 @@ import Button from "../components/Button";
 import { InputElement } from "../components/InputElement";
 import { User } from "../components/User";
 import { useState } from "react";
+import { SettingsForm } from "../components/SettingsForm";
+import { Message } from "../components/Message";
 
 export function ChatRoomPage(props) {
-    const [formState, setFormState] = useState({ message: '' });
-    const [messages, setMesages] = useState([]);
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-  
-      if (formState.message !== '') {
-        setMesages((state) => [...state,
-             { ...formState, autor: props.user.username}
-            ]);
-        setFormState({message: ''});
-      }
-    }
-    
-    const handleChange = (event) => {
-      setFormState((state) => ({
-            ...state,
-            [event.target.name]: event.target.value,
-      }));
-    }
+  const [formState, setFormState] = useState({ message: '' });
+  const [messages, setMessages] = useState([]);
+  const [settings, setSettings] = useState({});
 
-    const messageElements = messages.map((item, index) => <div key={index}>
-        <div>{item.autor}</div>
-        <div>{item.message}</div>
-        </div>)
-  
-    return (
-        <div>
-        <Button type="button" onClick={props.onSignOut}>Sign out</Button>
-        <User src="/avatar.jpg" username={props.user.username} />
-        <form className="form" onSubmit={handleSubmit}>
-          <div className="form-field">
-            <InputElement name="message" label="Message" type="text" onChange={handleChange}  value={formState.message}/>
-          </div>
-          <div className="form-field">
-            <Button type="submit">Send</Button>
-          </div>
-        </form>
-           {messageElements}
-      </div>
-    );
+  const updateSettings = (settings) => {
+    setSettings({ ...settings });
   }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (formState.message !== '') {
+      setMessages((state) => [
+        ...state,
+        {
+          ...formState,
+          author: props.user.username,
+          settings: { ...settings },
+        },
+      ]);
+      setFormState({ message: '' });
+    }
+  }
+
+  const handleChange = (event) => {
+    setFormState((state) => ({
+          ...state,
+          [event.target.name]: event.target.value,
+    }));
+  }
+
+  const messageElements = messages.map((item, index) => (
+    <Message
+      key={index}
+      settings={item.settings}
+      author={item.author}
+      message={item.message}
+    />
+  ));
+
+  return (
+    <div>
+      <Button type="button" onClick={props.onSignOut}>Sign out</Button>
+      <User src="/avatar.jpg" username={props.user.username} />
+
+      <SettingsForm user={props.user} onSubmit={updateSettings} />
+
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-field">
+          <InputElement name="message"
+            label="Message"
+            type="text"
+            onChange={handleChange}
+            value={formState.message} />
+        </div>
+        <div className="form-field">
+          <Button type="submit">Send</Button>
+        </div>
+      </form>
+      <div className="message-list">
+        {messageElements}
+      </div>
+    </div>
+  );
+}
